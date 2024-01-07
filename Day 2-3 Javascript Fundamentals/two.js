d208f1a8c5412acc0469120ff4a8ba9b66912ba9
Sanket Rajendra Shinde <126979961+sanketshinde3001@users.noreply.github.com>
2024-01-07 15:51:08 +0530

    Delete Day 2-3 Javascript Fundamentals directory

--- Day 2-3 Javascript Fundamentals/two.js
+++
@@ -1,9 +0,0 @@
-// Create a function calculateArea that takes the radius of a circle as a parameter, calculates its area, and then logs the result. Declare a variable radius and call the function with this variable.
-
-function calculateArea(r) {
-    const area  = Math.PI * r * r;
-    console.log(area);
-}
-
-const radius = 5;
-calculateArea(radius);
