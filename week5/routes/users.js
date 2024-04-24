b2fd767a0dfb8be2963e08a1239018b743c29b1c
Sanket Rajendra Shinde <126979961+sanketshinde3001@users.noreply.github.com>
2024-04-24 13:50:42 +0530

    week5

--- week5/routes/users.js
+++ week5/routes/users.js
@@ -1,9 +1,32 @@
 const mongoose = require( "mongoose");
 const plm = require("passport-local-mongoose");
 require('dotenv').config();
+mongoose.set("strictQuery", true);
+// async function connectToMongoDB(url) {
+//   return mongoose.connect(url);
+// }
 
-mongoose.connect(process.env.mongo_pass);
-// it will create the db name - practicekaro
+const connectToMongoDB = async (uri) => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+
+try {
+  connectToMongoDB(process.env.mongo_pass).then(() =>
+  console.log("Mongodb connected")
+);
+} catch (error) {
+  console.log(error);
+}
+
+
 
 const userschema = mongoose.Schema({
   username: String,
